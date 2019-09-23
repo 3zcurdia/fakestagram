80261381b6ed42e9aa89a736a0ce6b63fa10d445
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2019-09-23 18:06:03 -0500

    Fix rails workflow (#48)
    * specify ruby version
    
    * use ruby container
    
    * enable postgres service
    
    * setup db
    
    * split dependencies steps
    
    * change to postgres host
    
    * fix pg host on test
    
    * fix broken test

--- test/controllers/api/v1/accounts_controller_test.rb
+++ test/controllers/api/v1/accounts_controller_test.rb
@@ -10,7 +10,7 @@ class Api::V1::AccountsControllerTest < ActionDispatch::IntegrationTest
   test 'should create account' do
     params = { device_model: 'iPhone Xs', device_number: SecureRandom.uuid, name: 'Test User' }
     assert_difference('Account.count') do
-      post api_accounts_url, params: params, as: :json
+      post api_v1_accounts_url, params: params, as: :json
     end
 
     assert_response 201
