01ffc78d2f92d15a1ad82196322545b1aa8df291
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2019-09-23 23:16:01 -0500

    Disable action_mailbox and action_text

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,6 @@
 
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-
   allow do
     origins '*'
     resource '*', headers: :any, methods: %i[get post put options]
