d34e0f14eb1d39011a9eb7ee6ab286972e7da870
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2019-09-25 23:20:24 -0500

    include source url on post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,11 @@
 class PostSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :likes_count, :comments_count, :created_at, :updated_at, :location
-  belongs_to :account
-  has_many :likes
-  has_many :comments
+  belongs_to :account, lazy_load_data: true
+  has_many :likes, lazy_load_data: true
+  has_many :comments, lazy_load_data: true
+
+  attribute :image_url do |object|
+    object.image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(object.image, host: ENV['HOST']) : nil
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['HOST']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
