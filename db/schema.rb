07ae894fc92fc00d80c044ce8e44a012eaebcb29
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-02-16 17:38:48 -0600

    fix style on schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,43 +13,42 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_16_213250) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-  enable_extension "postgis"
+  enable_extension 'plpgsql'
+  enable_extension 'postgis'
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "post_id"
-    t.bigint "user_id"
-    t.text "content", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.bigint 'post_id'
+    t.bigint 'user_id'
+    t.text 'content', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.text "title", null: false
-    t.string "image"
-    t.string "background_color", default: "#333333"
-    t.integer "comments_count", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
-    t.string "ip_source"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+  create_table 'posts', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.text 'title', null: false
+    t.string 'image'
+    t.string 'background_color', default: '#333333'
+    t.integer 'comments_count', default: 0
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.geography 'lonlat', limit: { srid: 4326, type: 'st_point', geographic: true }
+    t.string 'ip_source'
+    t.index ['user_id'], name: 'index_posts_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "public_key"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "username", null: false
-    t.string "password_digest", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'public_key'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'username', null: false
+    t.string 'password_digest', null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key 'comments', 'posts'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'posts', 'users'
 end
